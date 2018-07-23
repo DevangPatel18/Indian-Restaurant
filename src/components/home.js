f3ddcd3765bd7f9963bb4464f1f60282f2b92c63
dev637 <devang713@gmail.com>
2018-07-23 13:41:43 -0400

    Add background to hero banner

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Indian Restaurant</h1>
-      </header>
+    <div>
+
+      <section className="main">
+        <div className="container">
+          <h1 className="title is-1">Indian Restaurant</h1>
+        </div>
+      </section>
+
     </div>
   )
 }
