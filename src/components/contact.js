b99af432316dafc2c7dce06600fee3511aeb57d2
dev637 <devang713@gmail.com>
2018-08-12 16:15:26 -0400

    Add banner image for contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./contact.css";
 
 const Contact = () => {
   return (
     <div>
-      <header className="centercontent">
+      <header className="contact centercontent">
         <h1 className="has-text-light">Contact</h1>
       </header>
     </div>
