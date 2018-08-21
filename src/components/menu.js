edf9074ae5cd8217f438bbcc41088fad959cfd9b
dev637 <devang713@gmail.com>
2018-08-21 11:06:26 -0400

    Convert menu csv to array of objects
    Use papaparse for handling csv to obj conversion. Also required to move csv file to public folder.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import './menu.css'
+import './menu.css';
+import Papa from "papaparse";
 
 const Menu = () => {
+  let food = []
+
+  fetch('menu.csv')
+    .then(response => response.text())
+    .then(csvtext => {
+      
+      Papa.parse(csvtext, {
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results) {
+          results.data.forEach(x => food.push(x))
+        }
+      });
+    })
+
   return (
     <div>
       <header className="menu centercontent">
