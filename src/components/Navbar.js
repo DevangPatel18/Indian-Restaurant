ff3c599f1e7f972131a0e8f8073558dd8335be2f
dev637 <devang713@gmail.com>
2018-08-30 14:19:42 -0400

    Restyle navbar
    Change background colors and logo, include logo credit in footer.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "./Navbar.css";
-import logo from "./Taj_Mahal_logo.png";
+import logo from "./taj-mahal.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar is-fixed-top is-light">
+    <nav className="navbar is-fixed-top">
       <div className="navbar-brand">
-        <img src={logo} alt="" style={{width: '50px', height:'50px'}}/>
+        <img src={logo} alt="" className="navbar-logo"/>
         <a role="button" className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-end">
-        <div className="navbar-menu is-light" id="navMenu">
+        <div className="navbar-menu" id="navMenu">
           <div className="navbar-item"><NavLink className="Nav_link" to="/">Home</NavLink></div>
           <div className="navbar-item"><NavLink className="Nav_link" to="/menu">Menu</NavLink></div>
           <div className="navbar-item"><NavLink className="Nav_link" to="/about">About</NavLink></div>
