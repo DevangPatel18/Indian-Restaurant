983bb29d3eec16db6f751b31c03ac46ce48c6bb4
dev637 <devang713@gmail.com>
2018-09-06 18:54:12 -0400

    Add responsiveness to contactinfo component
    Replace Ant design Row and Col with Bulma column layout for better responsiveness.

--- src/components/contactinfo.js
+++ src/components/contactinfo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './contactinfo.css';
-import {Row, Col} from 'antd';
 
 const Contactinfo = () => {
 
@@ -8,40 +7,25 @@ const Contactinfo = () => {
     <div className="contactinfo">
         
       <div className="contactinfo-hours">
-        <Row>
-          <Col className="contactinfo-hours-header has-text-centered has-text-weight-bold is-size-5">Hours</Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row gutter={18}>
-              <Col span={12} className="has-text-right"><p>Monday - Friday</p></Col>
-              <Col span={12} className="has-text-left"><p>10am-11pm</p></Col>
-            </Row>
-            <Row gutter={18}>
-              <Col span={12} className="has-text-right"><p>Saturday</p></Col>
-              <Col span={12} className="has-text-left"><p>11am-9pm</p></Col>
-            </Row>
-            <Row gutter={18}>
-              <Col span={12} className="has-text-right"><p>Sunday & Holidays</p></Col>
-              <Col span={12} className="has-text-left"><p>12pm-8pm</p></Col>
-            </Row>
-          </Col>
-        </Row>
+        <div className="columns is-multiline is-mobile">
+          <div className="column is-full contactinfo-hours-header has-text-centered has-text-weight-bold is-size-5">Hours</div>
+          <div className="column is-half has-text-right"><p>Monday - Friday</p></div>
+          <div className="column is-half has-text-left"><p>10am-11pm</p></div>
+          <div className="column is-half has-text-right"><p>Saturday</p></div>
+          <div className="column is-half has-text-left"><p>11am-9pm</p></div>
+          <div className="column is-half has-text-right"><p>Sunday & Holidays</p></div>
+          <div className="column is-half has-text-left"><p>12pm-8pm</p></div>
+        </div>
       </div>
 
-      <div>
-        <Row>
-          <Col className="has-text-centered has-text-weight-bold is-size-5">Contact info</Col>
-        </Row>
-        <Row gutter={24}>
-          <Col className="has-text-left">
-            <p><i className="is-size-3 fas fa-map-marked-alt"></i> 542 Nowaresville, Toronto, ON</p>
-            <p><i className="is-size-3 fas fa-phone"></i> 999-999-999</p>
-            <p><i className="is-size-3 far fa-envelope"></i> support@indianrest.com</p>
-          </Col>
-        </Row>
-      </div>  
-
+      <div className="contactinfo-contact">
+        <div className="columns is-multiline is-mobile">
+          <div className="column is-full has-text-centered has-text-weight-bold is-size-5">Contact info</div>
+            <div className="column is-full"><p><i className="is-size-3 fas fa-map-marked-alt"></i> 542 street, Toronto, ON</p></div>
+            <div className="column is-full"><p><i className="is-size-3 fas fa-phone"></i> 999-999-999</p></div>
+            <div className="column is-full"><p><i className="is-size-3 far fa-envelope"></i> support@indianrest.com</p></div>
+        </div>
+      </div>
     </div>
   )
 }
