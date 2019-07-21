25b09c384a097406f8805c63b134dfc898a92cf7
dev637 <devang713@gmail.com>
2019-07-21 12:24:57 -0400

    Update hamburger icon
    Updated hamburger icon to button element and removed redundant role. Also removed usual button styling to maintain original hamburger appearance.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,11 +24,11 @@ const Navbar = () => {
           </div>
         </div>
 
-        <a role="button" className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+        <button className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
-        </a>
+        </button>
       </div>
 
       <div className="navbar-end">
